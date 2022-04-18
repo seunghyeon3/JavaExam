5da0588ea770128960e80a80c540fd8d0556c30e
psh <pshandkyp@gmail.com>
2022-04-18 20:36:18 +0900

    123

---
+++ study/src/study/api.java
@@ -0,0 +1,43 @@
+package study;
+
+import java.util.Scanner;
+
+public class api {
+
+	public void test() {
+		
+	}
+	public static void main(String[] args) {
+	
+		
+		
+		Scanner sc = new Scanner(System.in);
+		int a;
+		String n ;
+		int b;
+		int c;
+		
+		while(true) {
+			System.out.println("입력하세요");
+			a =  sc.nextInt();
+			/* sc.nextLine(); */
+			sc.nextLine();
+			System.out.println("입력하세요2");
+			
+			n = sc.nextLine();
+			
+			System.out.println("입력하세요3");
+			n = sc.nextLine();
+			
+			
+			if(a == 5) {
+				break;
+			}
+			
+		}
+		
+		
+		
+	}
+
+}
