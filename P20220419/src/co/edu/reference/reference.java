5beecb350d5c0d0a473e4d447d5175884165c7f4
admin <admin@YD4-00>
2022-04-19 15:40:09 +0900

    220419

---
+++ P20220419/src/co/edu/reference/reference.java
@@ -0,0 +1,21 @@
+package co.edu.reference;
+
+public class reference {
+	public static void main(String[] args) {
+		
+		String a = "자바";
+		String b = "자바";
+		if(a==b) {
+			System.out.println("a");
+		}
+		if(a.equals(b)) {
+			System.out.println("b");
+		}
+				
+				 
+		
+		
+		
+		
+	}
+}
